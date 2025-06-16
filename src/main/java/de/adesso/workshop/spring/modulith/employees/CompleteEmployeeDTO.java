652014659b1d66055e72307cb1c44a0f462944c3
denwae <43241874+denwae@users.noreply.github.com>
2025-06-16 22:13:52 +0200

    removed dead code

--- src/main/java/de/adesso/workshop/spring/modulith/employees/CompleteEmployeeDTO.java
+++ src/main/java/de/adesso/workshop/spring/modulith/employees/CompleteEmployeeDTO.java
@@ -3,8 +3,6 @@ package de.adesso.workshop.spring.modulith.employees;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import de.adesso.workshop.spring.modulith.employees.internal.foreign.ExternalCompany;
-
 public record CompleteEmployeeDTO(
     UUID id,
     String firstName,
@@ -14,28 +12,9 @@ public record CompleteEmployeeDTO(
     EmployerDto employer
 ) {
 
-    public static CompleteEmployeeDTO from(Employee employee, ExternalCompany employer) {
-        return new CompleteEmployeeDTO(
-            employee.getId().id(),
-            employee.getFirstName(),
-            employee.getLastName(),
-            employee.getEmail(),
-            employee.getDateOfBirth(),
-            EmployerDto.from(employer)
-        );
-    }
-
     public record EmployerDto(
         UUID id,
         String name,
         String address
-    ) {
-        public static EmployerDto from(ExternalCompany employer) {
-            return new EmployerDto(
-                employer.getId().id(),
-                employer.getName(),
-                employer.getAddress()
-            );
-        }
-    }
+    ) { }
 }
